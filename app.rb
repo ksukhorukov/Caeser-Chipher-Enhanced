6b10891649c3441c258f63b2515d5b9cfeef835b
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-12-10 00:53:24 +0800

    params check

--- app.rb
+++ app.rb
@@ -39,7 +39,7 @@ class CaesarChipher
     end
 
     return (current_idx + shift_n) % dictionary.size if encode == true
-    
+
     return ((current_idx - shift_n) % dictionary.size).abs
   end
 
@@ -116,6 +116,18 @@ end
 @input_file_path = ARGV[1] || INPUT_FILE_PATH 
 @output_file_pathh = ARGV[2] || OUTPUT_FILE_PATH
 @shift_n = ARGV[3]&.to_i || DEFAULT_SHIFT_N 
+
+def usage
+  puts "\n#{$0} encode|decpde ./input.txt ./output.txt 5\n\nThe last param is shifting number, e.g. 25\n".colorize(:green)
+  exit
+end 
+
+def correct_params?
+  ARGV[0] == 'encode' || ARGV[0] == 'decode'
+end
+
+usage if ARGV.size < 1 
+usage unless correct_params?
 
 CaesarChipher.new(
     input: @input_file_path, 
